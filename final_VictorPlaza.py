d48aa767e636e1973d51f0d5216065e2941a63f9
vmplaza <87671925+vmplaza@users.noreply.github.com>
2021-07-19 16:23:49 -0400

    Update final_VictorPlaza.py

--- final_VictorPlaza.py
+++ final_VictorPlaza.py
@@ -1,17 +1,3 @@
-"""
-Name: Victor Plaza
-CS600: SN 1
-Data: Volcano dataset
-URL: Link to your web application online (see extra credit)
-Description: This program will provide a brief introduction to the different volcanoes around the world
-in addition to global maps that pinpoint to specific regions/countries in which the user will be able to locate.
-It also provides pie charts with the different types of volcanoes in each country with the possibility of a brief explanation
-on these different types. It will also provide a bar chart demonstrating the tallest volcanoes in teh country selected.
-Finally, this program permits a simple comparative analysis between three distinct countries and their maximum,
-average, and minimum volcanic elevation.
-
-Enjoy!
-"""
 import matplotlib
 import matplotlib.pyplot as plt
 import streamlit as st
